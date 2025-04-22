dd1079ecb8a753948fee6f9d7c7bef3afb7f635b
AZO234 <typesylph@gmail.com>
2025-04-22 14:41:34 +0900

    getjst.php

--- src/getjst/getjst.ts
+++ src/getjst/getjst.ts
@@ -1,51 +1,51 @@
 /*****
-日本標準時(JST)取得 getjst.cgi使用
+日本標準時(JST)取得 getjst.php使用
 Written by AZO
 
-getjst.cgiは、日本標準時(JST)を、
+getjst.phpは、日本標準時(JST)を、
 NICT(情報通信研究機構)が配信している
 NTPによる時刻情報を取得し、
 修正したサーバ時刻をJSON形式にて返答するAPIである。
-URL: https://domisan.sakura.ne.jp/getjst.cgi
+URL: https://domisan.sakura.ne.jp/getjst.php
 
 // getjst.cgiが返答するJSONデータ
 {
   "result": int 0:失敗/1:成功、NTP取得にて時刻修正/2:成功、キャッシュにて時刻修正,
-  "rth": 32bit 受信UNIX時刻 整数部,
-  "rtl": 32bit 受信UNIX時刻 小数部,
-  "tth": 32bit 送信UNIX時刻 整数部,
-  "ttl": 32bit 送信UNIX時刻 小数部
-},
+  "t1h": 32bit クライアント開始UNIX時刻 整数部,
+  "t1l": 32bit クライアント開始UNIX時刻 小数部,
+  "t2h": 32bit サーバ受信UNIX時刻 整数部,
+  "t2l": 32bit サーバ受信UNIX時刻 小数部,
+  "t3h": 32bit サーバ返信UNIX時刻 整数部,
+  "t3l": 32bit サーバ返信UNIX時刻 小数部,
+  "t4h": 32bit クライアント終了UNIX時刻 整数部,
+  "t4l": 32bit クライアント終了UNIX時刻 小数部
+}
 
 // 時刻補正方法
-lt1 ローカル側送信時刻
-rt  サーバ側受信時刻
-tt  サーバ側送信時刻
-lt2 ローカル側受信時刻
-delta ローカルの遅延 = ((rt + tt) - (lt1 + lt2)) / 2
-fixlt = lt + delta
+delta ローカルの遅延 = ((t2 + t3) - (t1 + t4)) / 2
+fix_time = localtime + delta
 *****/
 
 // 日本標準時(JST)取得クラス
 export default class getjst {
   constructor() {
     const gj = this;
-    const lt1 = (new Date()).getTime();
-    fetch('https://domisan.sakura.ne.jp/getjst.cgi')
+    const t1 = (new Date()).getTime();
+    fetch('https://domisan.sakura.ne.jp/getjst.php')
       .then((res) => {
         if(res.status === 200) {
-          const lt2 = (new Date()).getTime();
+          const t4 = (new Date()).getTime();
           res.json()
             .then((json) => {
-              let rtl = json.rtl;
-              let ttl = json.ttl;
+              let t2l = json.t2l;
+              let t3l = json.t3l;
               for(var i = 0; i < 32; i++) {
-                rtl /= 2;
-                ttl /= 2;
+                t2l /= 2;
+                t3l /= 2;
               }
-              const rth = (json.rth + rtl) * 1000;
-              const tth = (json.tth + ttl) * 1000;
-              gj._delta = ((rth + tth) - (lt1 + lt2)) / 2;
+              const t2 = (json.t2h + t2l) * 1000;
+              const t3 = (json.t3h + t3l) * 1000;
+              gj._delta = ((t2 + t3) - (t1 + t4)) / 2;
             });
         }
       });
